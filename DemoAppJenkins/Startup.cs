5b2fd502387a21cc9027e589889a55eba2aeec04
Maisam Razai <Maisam.Razai@quest.com>
2022-07-13 11:18:54 -0400

    test

--- DemoAppJenkins/Startup.cs
+++ DemoAppJenkins/Startup.cs
@@ -24,6 +24,7 @@ namespace DemoAppJenkins
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            // test
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
